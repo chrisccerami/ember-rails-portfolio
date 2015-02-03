f0019f78b1b43968801efb30e6f420c685bd14f4
Chris C Cerami <chrisccerami@gmail.com>
2015-02-02 20:23:41 -0500

    Created_at and updated_at now being properly permitted in params, and displaying on post show page

--- rails/app/controllers/api/posts_controller.rb
+++ rails/app/controllers/api/posts_controller.rb
@@ -37,6 +37,6 @@ class Api::PostsController < ApplicationController
   private
 
   def post_params
-    params.require('post').permit(:title, :body)
+    params.require('post').permit(:title, :body, :created_at, :updated_at)
   end
 end
